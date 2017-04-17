ea97c70a975a52581071132dffc84a741e18cc49
vgvoleg <vgvoleg@mail.ru>
2017-04-17 09:53:02 +0300

    Empty DifferentialEvolution added

---
+++ src/main/java/com/vgvoleg/heuristic/algorithms/DifferentialEvolution.java
@@ -0,0 +1,25 @@
+package com.vgvoleg.heuristic.algorithms;
+
+import com.vgvoleg.heuristic.problems.base.*;
+
+public class DifferentialEvolution extends HeuristicAlgorythm
+{
+    // TODO: realise it
+
+    public DifferentialEvolution(OptimizationProblem problem, OptimizationType type)
+    {
+        super(problem, type);
+    }
+
+    @Override
+    public OptimizationResult findResult()
+    {
+        return null;
+    }
+
+    @Override
+    public OptimizationDetailedResult findDetailedResult()
+    {
+        return null;
+    }
+}
